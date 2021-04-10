3bfeaf06a2d99ef79f6b6f8cc13d0b7e770a0106
Maxx <maxhiga.hawaii@gmail.com>
2021-04-09 18:14:24 -1000

    FINISHED transactionModel.js

--- models/transactionModel.js
+++ models/transactionModel.js
@@ -1,5 +1,22 @@
 const mongoose = require("mongoose");
 
-const transactionSchema = new mongoose.Schema({});
+const transactionSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    trim: true,
+    required: "Enter description of transaction",
+  },
+  value: {
+    type: Number,
+    required: "Enter transaction amount",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  filter: {
+    type: Array,
+  },
+});
 
 module.exports = Transaction = mongoose.model("transaction", transactionSchema);
