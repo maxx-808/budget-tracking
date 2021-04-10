d185acc63783f2dad91d960f9e10648b58362ad0
Maxx <maxhiga.hawaii@gmail.com>
2021-04-09 18:23:26 -1000

    FINISHED userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,38 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  fName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Enter a valid email"],
+  },
+  phoneNum: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /\d{3}-\d{3}-\d{4}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
 
 module.exports = User = mongoose.model("user", userSchema);
