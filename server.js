7ed380f736d12fb206b4ee26bc55d042d7c7a39a
Maxx <maxhiga.hawaii@gmail.com>
2021-04-09 17:54:03 -1000

    added server and installed packages

---
+++ server.js
@@ -0,0 +1,37 @@
+//server dependencies
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5000;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//mongoose connection
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budgetTracking",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("connected to mongodb");
+  }
+);
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+}
+
+//calling the routes
+// app.use("/api/accounts", require("./routes/accountRoutes"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
