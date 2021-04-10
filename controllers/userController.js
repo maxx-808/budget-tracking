48f51ad54114672ec57ee3fba785d52c06e042a8
Maxx <maxhiga.hawaii@gmail.com>
2021-04-09 18:48:17 -1000

    ADDED login func for userController && STARTED routing

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,12 +23,12 @@ module.exports = {
           .json({ msg: "Password does not match the password check." });
       }
 
-      const existingEmail = Account.findOne({ email: email });
+      const existingEmail = User.findOne({ email: email });
       if (existingEmail) {
         return res.status(400).json({ msg: "The email is already in use" });
       }
 
-      //creating salt (random string) to encrypt account password to safeguard users accounts
+      //creating salt (random string) to encrypt user password to safeguard users information
       const salt = await bcrypt.genSalt(20);
       //hashing (mixing) the salt and password so that it is random string
       const passwordHash = await bcrypt.hash(password, salt);
@@ -43,5 +43,42 @@ module.exports = {
     } catch (err) {
       res.status(500).json({ msg: err });
     }
+  },
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      if (!email) {
+        res.status(400).json({ msg: "Please enter an email" });
+      }
+      if (!password) {
+        res.status(400).json({ msg: "Please enter a password" });
+      }
+
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        res.status(401).json({
+          msg: "The email or password you have entered is incorrect",
+        });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        res
+          .status(401)
+          .json({ msg: "The email or password you have entered is incorrect" });
+      }
+      const token = jwt.sign({ id: userContext._id }, process.env.JWT_SECRET, {
+        expiresIn: "24h",
+      });
+
+      res.json({
+        token,
+        user: {
+          id: user._id,
+          email: user.email,
+          name: [user.fName, user.lName],
+        },
+      });
+    } catch (err) {}
   },
 };
