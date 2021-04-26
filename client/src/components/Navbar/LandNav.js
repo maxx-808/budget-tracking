14117e57d85bb815f4fd71f9d1a7c5678e1ee788
Maxx <maxhiga.hawaii@gmail.com>
2021-04-25 17:02:16 -1000

    added navbar for landing page area

---
+++ client/src/components/Navbar/LandNav.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "../../Pages/Login";
+import "../../Pages/Register";
+
+function LandNav(props) {
+  const currentPage = props.page[0].page;
+
+  useEffect(() => {
+    const login = document.getElementById("login");
+    const register = document.getElementById("register");
+    if (currentPage === "/login") {
+      login.classList.add("hidden");
+    } else if (currentPage === "/register") {
+      register.classList.add("hidden");
+    }
+  });
+
+  return (
+    <div className="nav-wrapper">
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li id="login">
+          <Link to="/login">Login</Link>
+        </li>
+        <li id="register">
+          <Link to="/register">Register</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default LandNav;
