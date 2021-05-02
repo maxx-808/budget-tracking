3094d719ffacd7117ea42b839e568bd85faf697a
Maxx <maxhiga.hawaii@gmail.com>
2021-05-01 21:42:06 -1000

    finishing skeleton of contact page and functions

--- client/src/Pages/Contact.js
+++ client/src/Pages/Contact.js
@@ -3,13 +3,71 @@ import UserContext from "../Context/UserContext";
 import { useHistory } from "react-router-dom";
 import LandNav from "../components/Navbar/LandNav/LandNav";
 import "../App.css";
+import axios from "axios";
 
 const Contact = () => {
   const page = useState({ page: "contact" });
+  const [contact, setContact] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  console.log(contact);
+
+  const onChange = (e) => {
+    setContact({ ...contact, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e, req, res) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("/api/contact/send", {
+        data: contact,
+      });
+      res.status(200).json({ msg: "Your message has been sent." });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div>
       <LandNav page={page} />
+      <form id="contact-form" onSubmit={handleSubmit} method="POST">
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            name="name"
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            aria-describedby="emailHelp"
+            name="email"
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea
+            className="form-control"
+            rows="5"
+            name="message"
+            onChange={onChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
