3417b3409c0232660fe13e37e61c7a67e9002972
Maxx <maxhiga.hawaii@gmail.com>
2021-05-07 01:21:03 -1000

    logging errs and finding where the err starts in the line of transactions

--- client/src/components/Services/GetTrans.js
+++ client/src/components/Services/GetTrans.js
@@ -2,13 +2,15 @@ import axios from "axios";
 
 const API_URL = "http://localhost:3000/api/transactions/";
 
-const GetTrans = async (userId) => {
+const GetTrans = async (stuff) => {
+  let cancelToken = axios.CancelToken;
+  let source = cancelToken.source();
   try {
-    const all = await axios.get(API_URL, userId);
-    if (!all) {
-      return null;
-    }
-    return all.data;
+    const all = await axios.get(API_URL, {
+      cancelToken: source.token,
+      params: stuff,
+    });
+    return all.data.allUsersTrans;
   } catch (err) {
     return err;
   }
